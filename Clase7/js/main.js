b8fd99c524b380f573ee3fad1cf24c521334bfa2
Sanpa33 <127140452+Sanpa33@users.noreply.github.com>
2024-12-24 13:39:17 -0300

    Agregar funciones de validación y manejo de errores
    - Añadidas funciones para validar los campos de nombre, ciudad y descripción del regalo.
    - Implementada la función `manejarErrores` para gestionar y mostrar los mensajes de error correspondientes.
    - Evitar el envío del formulario si hay errores de validación.
    - Mostrar mensajes de error en la interfaz de usuario.

--- Clase7/js/main.js
+++ Clase7/js/main.js
@@ -8,10 +8,43 @@
 
 const $form = document.querySelector('#carta-a-santa');
 
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form['descripcion-regalo'].value;
+function validarFormulario(event) {
+
+  event.preventDefault();
+
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form['descripcion-regalo'].value;
+  
+  const errorNombre = validarNombre(nombre);
+  const errorCiudad = validarCiudad(ciudad);
+  const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+  
+  const errores = {
+    nombre: errorNombre,
+    ciudad: errorCiudad,
+    'descripcion-regalo': errorDescripcionRegalo,
+  };
+
+  manejarErrores(errores);
+
+  // const esExito = manejarErrores(errores) === 0;
+  
+  // if (esExito) {
+  //   $form.className = 'oculto';
+  //   document.querySelector('#exito').className = '';
+  //   setTimeout(() => {
+  //     window.location.href = 'wishlist.html';
+  //   }, 5000);
+  // }
+
+  console.log(nombre);
+  console.log(ciudad);
+  console.log(comportamiento);
+  console.log(descripcionRegalo);
+
+}
+
 
 function validarNombre(nombre) {
   
@@ -67,3 +100,32 @@ function validarDescripcionRegalo(descripcionRegalo) {
 
 }
 
+function manejarErrores(errores) {
+
+  errorNombre = errores.nombre;
+  errorCiudad = errores.ciudad;
+  errorDescripcionRegalo = errores['descripcion-regalo'];
+
+
+  if (errorNombre) {
+    $form.nombre.className = 'error';
+  } else {  
+    $form.nombre.className = '';
+  }
+
+  if (errorCiudad){
+    $form.ciudad.className = 'error';
+  } else{
+    $form.ciudad.className = '';
+  }
+
+  if(errorDescripcionRegalo){
+    $form["descripcion-regalo"].className = 'error';
+  } else{
+    $form["descripcion-regalo"].className = '';
+  }
+  
+
+}
+
+$form.onsubmit = validarFormulario;
